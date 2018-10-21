6f6bfd540c88f68f5fcdab450802e53658d44b97
Websitescity <websitescity@gmail.com>
2018-10-21 22:43:46 +0200

    Import my-component (navbar) in Vue project

--- vue-and-stencil/src/main.ts
+++ vue-and-stencil/src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { defineCustomElements as defineMyComponent } from "my-component-stencil/dist/loader";
 
+defineMyComponent(window);
+Vue.config.ignoredElements = [/^my-/];
 Vue.config.productionTip = false
 
 new Vue({
