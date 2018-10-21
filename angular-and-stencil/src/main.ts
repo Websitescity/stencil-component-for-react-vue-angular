9e7e3910c4c0711c72af33b18b9a59595930e684
Websitescity <websitescity@gmail.com>
2018-10-21 22:44:20 +0200

    Import my-component (navbar) in Angular project

--- angular-and-stencil/src/main.ts
+++ angular-and-stencil/src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements as defineMyComponent } from 'my-component-stencil/dist/loader';
+
+defineMyComponent(window);
 
 if (environment.production) {
   enableProdMode();
