24dbba9073459d01a430b0e5d983a0af9a89611f
Websitescity <websitescity@gmail.com>
2018-10-21 22:44:36 +0200

    Import my-component (navbar) in React project

--- react-and-stencil/src/App.js
+++ react-and-stencil/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <my-component first="Stencil menu"></my-component>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
